d53b9c617cd5c249dc0855068f4db66875a90163
CelesteCoopa <celeste.rcl@gmail.com>
2013-09-29 04:18:02 -0300

    Revert "Desarrollo Clase Mano y Testeo"
    This reverts commit ed15b2a0e0da0460410c89c325048586f99037e6.

--- src/main/java/edu/tallerweb/pptls/Mano.java
+++ src/main/java/edu/tallerweb/pptls/Mano.java
@@ -4,44 +4,24 @@ package edu.tallerweb.pptls;
  * Representa una de las Manos involucradas en el juego
  */
 public class Mano {
-	private Forma forma;
-	private Integer mat[][] = { { 0, 2, 2, 1, 1 }, { 1, 0, 2, 2, 1 },
-			{ 1, 1, 0, 2, 2 }, { 2, 1, 1, 0, 2 }, { 2, 2, 1, 1, 0 } };
 
 	/**
-	 * Toda Mano debe crearse con una forma dada, que será la que determine su
-	 * condición en el juego.
-	 * 
-	 * @param forma
-	 *            , la Forma que adopta la Mano.
+	 * Toda Mano debe crearse con una forma dada, que será
+	 * la que determine su condición en el juego.
+	 * @param forma, la Forma que adopta la Mano.
 	 */
-
 	public Mano(final Forma forma) {
-		this.forma = forma;
+		throw new RuntimeException("No implementado aún");
 	}
 
 	/**
-	 * Evaluará el resultado de la partida según las reglas del juego.
-	 * 
-	 * @param otra
-	 *            , la otra Mano.
+	 * Evaluará el resultado de la partida según las reglas
+	 * del juego.
+	 * @param otra, la otra Mano.
 	 * @return un Resultado, de acuerdo al estado del juego.
 	 */
-
 	public Resultado jugarCon(final Mano otra) {
-
-		int res = mat[forma.getValor()][otra.forma.getValor()];
-
-		switch (res) {
-		case 0:
-			return Resultado.EMPATA;
-		case 1:
-			return Resultado.GANA;
-		case 2:
-			return Resultado.PIERDE;
-		}
-		return null;
-
+		throw new RuntimeException("No implementado aún");
 	}
 
 }
