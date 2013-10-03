d324440b459c773f92f6d3c8513152ec501e1847
CelesteCoopa <celeste.rcl@gmail.com>
2013-10-02 21:30:33 -0300

    Revert "Resolucion"
    This reverts commit 43566c2164f081517dc23c3a5e01afba768ecc55.

--- src/main/java/edu/tallerweb/pptls/Mano.java
+++ src/main/java/edu/tallerweb/pptls/Mano.java
@@ -4,24 +4,44 @@ package edu.tallerweb.pptls;
  * Representa una de las Manos involucradas en el juego
  */
 public class Mano {
+	private Forma forma;
+	private Integer mat[][] = { { 0, 2, 2, 1, 1 }, { 1, 0, 2, 2, 1 },
+			{ 1, 1, 0, 2, 2 }, { 2, 1, 1, 0, 2 }, { 2, 2, 1, 1, 0 } };
 
 	/**
-	 * Toda Mano debe crearse con una forma dada, que será
-	 * la que determine su condición en el juego.
-	 * @param forma, la Forma que adopta la Mano.
+	 * Toda Mano debe crearse con una forma dada, que será la que determine su
+	 * condición en el juego.
+	 * 
+	 * @param forma
+	 *            , la Forma que adopta la Mano.
 	 */
+
 	public Mano(final Forma forma) {
-		throw new RuntimeException("No implementado aún");
+		this.forma = forma;
 	}
 
 	/**
-	 * Evaluará el resultado de la partida según las reglas
-	 * del juego.
-	 * @param otra, la otra Mano.
+	 * Evaluará el resultado de la partida según las reglas del juego.
+	 * 
+	 * @param otra
+	 *            , la otra Mano.
 	 * @return un Resultado, de acuerdo al estado del juego.
 	 */
+
 	public Resultado jugarCon(final Mano otra) {
-		throw new RuntimeException("No implementado aún");
+
+		int res = mat[forma.getValor()][otra.forma.getValor()];
+
+		switch (res) {
+		case 0:
+			return Resultado.EMPATA;
+		case 1:
+			return Resultado.GANA;
+		case 2:
+			return Resultado.PIERDE;
+		}
+		return null;
+
 	}
 
 }
